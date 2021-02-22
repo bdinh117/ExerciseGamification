5b59adbbaaa6f2419989d187d9376fef23d33be6
ayoub <ayoubhn@outlook.com>
2021-02-22 14:38:55 -0500

    initial setup

---
+++ gamefi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GamefiConfig(AppConfig):
+    name = 'gamefi'
---
+++ gamefi/views.py
@@ -0,0 +1,15 @@
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from django.utils import timezone
+
+
+def home(request):
+    return render(request, 'gamefi/index.html')
+
