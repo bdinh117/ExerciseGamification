31872aec35e029d5ee5b0e3f34f52d869b802b74
ayoub <56207828+ayoubnur@users.noreply.github.com>
2021-04-05 21:59:53 -0400

    update tests.py

--- gamifi/tests.py
+++ gamifi/tests.py
@@ -12,7 +12,7 @@ class testIfProfileIsCreated(TestCase):
     def testProfileExists(self):
         user = User.objects.get(username="Player1")
         self.assertEqual(str(user.profile),"Player1 Profile")
-'''
+
 
 class testIfProfileIsDeleted(TestCase):
     def setUp(self):
@@ -29,4 +29,4 @@ class AddGoalFormTest(TestCase):
         self.assertTrue(form.is_valid())
         form.save()
         self.assertTrue(Goal.objects.filter(title="Goal1",text="this is a goal").exists())
-
+'''
