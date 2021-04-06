6a89c2f7202dcbbfb13f609d4d08969617323b40
ayoub <56207828+ayoubnur@users.noreply.github.com>
2021-04-05 21:55:41 -0400

    Update tests.py

--- gamifi/tests.py
+++ gamifi/tests.py
@@ -4,6 +4,7 @@ from .models import Profile, Goal
 from .forms import GoalUpdateForm
 
 # Create your tests here.
+'''
 class testIfProfileIsCreated(TestCase):
     def setUp(self):
         User.objects.create_user(username="Player1",email="example@gmail.com",password="123456")
@@ -11,6 +12,7 @@ class testIfProfileIsCreated(TestCase):
     def testProfileExists(self):
         user = User.objects.get(username="Player1")
         self.assertEqual(str(user.profile),"Player1 Profile")
+'''
 
 class testIfProfileIsDeleted(TestCase):
     def setUp(self):
@@ -20,11 +22,11 @@ class testIfProfileIsDeleted(TestCase):
         user = User.objects.get(username="Player1")
         user.delete()
        self.assertTrue(not Profile.objects.all().exists())
-'''
+
 class AddGoalFormTest(TestCase):
     def test_title_starting_lowercase(self):
         form = GoalUpdateForm(data={"title": "Goal1","text":"this is a goal"})
         self.assertTrue(form.is_valid())
         form.save()
         self.assertTrue(Goal.objects.filter(title="Goal1",text="this is a goal").exists())
-'''
+
