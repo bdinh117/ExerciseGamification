53abe93c0e737d68e363bdcc3740f0dfc3e585f4
ayoub <ayoubhn@outlook.com>
2021-04-11 22:50:40 -0400

    Revert "added friends feature"
    This reverts commit 65485d0495ff9b022e6799040d6af1a4a013bf16.

--- gamifi/urls.py
+++ gamifi/urls.py
@@ -8,9 +8,7 @@ app_name = 'gamifi'
 urlpatterns = [
     path("", views.home, name="home"),
     path('profile/', views.profile,name="profile"),
-    path('editprofile/', views.edit_profile,name="edit-profile"),
-    path('send_friend_request/<int:userID>/',views.send_friend_request, name='send friend request'),
-    path('accept_friend_request/<int:userID>/',views.accept_friend_request, name='accept friend request'),
+    path('editprofile/', views.edit_profile,name="edit-profile")
 ]
 
 if settings.DEBUG:
