a4d78cf37e166a0d95e0422b696d5886027171f6
andrewncho <77546000+andrewncho@users.noreply.github.com>
2021-04-25 19:03:51 -0400

    Update urls.py

--- groupA10Site/urls.py
+++ groupA10Site/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
 
+
 urlpatterns = [
     path('', include('gamifi.urls')),
     path('gamifi/', include('gamifi.urls')),
