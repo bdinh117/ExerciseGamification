bc1ae02e407d123920ec28f11cb98acbd83941de
andrewncho <77546000+andrewncho@users.noreply.github.com>
2021-04-28 14:53:43 -0400

    Update admin.py

--- gamifi/admin.py
+++ gamifi/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Profile, FriendRequest,Comment, ExerciseChoice, Exercise
 # Register your models here.
 
+
 admin.site.register(Profile)
 admin.site.register(Comment)
 admin.site.register(ExerciseChoice)
