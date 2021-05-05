5375d8cfd45b1e05298d04e32faf0d3a14fdafa2
bdinh117 <61920809+bdinh117@users.noreply.github.com>
2021-05-05 17:31:35 -0400

    Update settings.py

--- groupA10Site/settings.py
+++ groupA10Site/settings.py
@@ -93,6 +93,11 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 
 
 # Password validation
